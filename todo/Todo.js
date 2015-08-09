eee2b5c477234eec1b38827cbb1a880788186273
hokoyama <hokoyama@mail>
2015-08-09 14:56:59 +0900

    追加

--- todo/Todo.js
+++ todo/Todo.js
@@ -1,12 +1,14 @@
 var TodoApp = angular.module('TodoApp', []);
 
-TodoApp.controller('TodoCtrl', ['$scope', function($scome){
+TodoApp.controller('TodoCtrl', ['$scope', function($scope){
 	$scope.todos= [];
 	index = 0;
 
 	$scope.add = function(){
 		$scope.todos.push({id: index, message: $scope.message, done: false});
+		//alert($scope.todos.message);
 		$scope.message = "";
+
 		index++;
 	};
 }]);
